67d30704a13943e59722cd3de5ca949cf3cbaea3
DonAthelion <gilberto.barrios.flores@gmail.com>
2025-09-30 11:02:10 -0700

    Update route.js

--- app/api/uxcoins/route.js
+++ app/api/uxcoins/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { getPool } from "@/lib/db";
+import { getPool } from "../../../lib/db.js";
 
 export async function GET(req) {
   const citizenid = req.nextUrl.searchParams.get("citizenid") || "";
