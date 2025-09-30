80c929b541bf5e831171b422baad398e0485429a
DonAthelion <gilberto.barrios.flores@gmail.com>
2025-09-30 11:02:28 -0700

    Update route.js

--- app/api/vip/route.js
+++ app/api/vip/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { getPool } from "@/lib/db";
+import { getPool } from "../../../lib/db.js";
 
 export async function GET() {
   const pool = getPool();
